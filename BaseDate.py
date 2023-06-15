ae19e367333977f57630414cad91a5f347c03cef
yungerprogramist <alekseiviskovath228@mail.ru>
2023-06-15 20:47:45 +0500

    123

--- BaseDate.py
+++ BaseDate.py
@@ -1,7 +1,7 @@
 import sqlite3
 db=sqlite3.connect('DataBases/workers.db',check_same_thread=False)
 sql=db.cursor()
-
+db.row_factory=sqlite3.Row
 # вставлять везде этот обработчик ошибок
 # db = None
 # try: 
@@ -64,9 +64,7 @@ def load_worker(date, username):
     finally: 
         db.commit()
         if db: db.close()
-
-
-def reg_client(client_info,username):
+def reg_client(client_info,username):               #функция записи заказчика в базу данных, принимает словарь и имя пользователя
         
     username=client_info[username]['username']
     chat_id=client_info[username]['chat_id']
@@ -77,8 +75,8 @@ def reg_client(client_info,username):
             return True
         else: #если такой записи нет  
             sql.execute(f"INSERT INTO client(name,phone_number,chat_id,username) VALUES('{name}','{phone_number}','{chat_id}','{username}')")
-            db.commit()
-            sql.close()
+            sql.execute(f"UPDATE main_info SET client_count = client_count + 1")
+            
             return False
     except sqlite3.Error as e: 
         if db: db.rollback()  
@@ -86,5 +84,12 @@ def reg_client(client_info,username):
  
  
     finally:  
+        db.commit()
         if db: db.close()
 
+
+
+def check_role(username):
+    sql.execute(f"SELECT 'worker' as source, username FROM worker WHERE username='{username}' UNION ALL SELECT 'client' as source, username FROM client WHERE username='{username}'")
+    rows = sql.fetchone()
+    return rows[0]
